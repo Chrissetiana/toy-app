864b7f2f3df9b2d0cca7adb54385aeb20b3b290a
chrissetiana <chrissetiana@gmail.com>
2018-07-27 21:27:52 +0100

    Added toy list

--- app/src/main/java/com/chrissetiana/toyapp/MainActivity.java
+++ app/src/main/java/com/chrissetiana/toyapp/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView toyList = findViewById(R.id.toy_names);
+        String[] toyNames = ToyBox.getToyNames();
+
+        for (String toyName : toyNames) {
+            toyList.append(toyName + "\n\n\n");
+        }
     }
 }
